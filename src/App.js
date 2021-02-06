c164340a95077e12aa9041c1b53d4189dcc3c00c
bagpyp <rtc@bagpyp.net>
2021-02-05 20:04:24 -0800

    add time to comments

--- src/App.js
+++ src/App.js
@@ -4,38 +4,51 @@ import { useState, useEffect } from 'react';
 
 
 function App() {
+  // CHANGE TO api.bagpyp.net after pointed
+  // const COMMENTS_API_URL = 'http://localhost:5000/comments'
+  const COMMENTS_API_URL = 'https://bagpyp-api.herokuapp.com/comments'
+  const [comments, setComments] = useState({})
+  const [values, setValues] = useState([])
+  const getComments = async () => {
+    const res = await fetch(COMMENTS_API_URL)
+    const json_res = await res.json()
+    setComments(json_res)
+  }
+  const postComment = async (data) => {
+    const options = {
+      method:'POST',
+      body: JSON.stringify(data)
+    }
+    const res = await fetch(COMMENTS_API_URL, options)
+  }
+
+  useEffect(() => {
+    getComments()
+  }, [])
 
   function Comments() {
-    // CHANGE TO api.bagpyp.net after pointed
-    const COMMENTS_API_URL = 'https://bagpyp-api.herokuapp.com/comments'
-    const [comments, setComments] = useState({})
-
-    const getComments = async () => {
-      const res = await fetch(COMMENTS_API_URL)
-      const json_res = await res.json()
-      setComments(json_res)
-    }
-
-    useEffect(() => {
-      getComments()
-    }, [comments])
-
     return (
       <div className="Comments">
         {Object.keys(comments).map((key, index) => (
           <div key={index}>
-            <h3>{comments[key]["name"]}</h3>
-            <p>{comments[key]["comment"]}</p>
+            <div className = "Comments-name">{comments[key]["name"]}</ div>
+            <div className ="Comments-comment">{comments[key]["comment"]}</ div>
+            <div className = "Comments-created_date">{new Date(Date.parse(comments[key]["created_date"])).toLocaleString(navigator.language)}</div>
           </div>
         ))}
       </div>
     );
   }
 
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>BAGPYP</h1>
       </header>
       <Comments />
     </div>
