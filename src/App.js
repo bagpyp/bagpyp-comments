9c04d040c438b4b4de961c35c6f9e7d4868d7dfe
bagpyp <rtc@bagpyp.net>
2021-02-07 13:38:36 -0800

    put comment form inside comment component

--- src/App.js
+++ src/App.js
@@ -18,6 +18,42 @@ function App() {
       getComments()
     }, [])
 
+    function CommentForm() {
+      const [comment_data, setComment_data] = useState({})
+      const postComment = async (data) => {
+        const options = {
+          method: 'POST',
+          body: JSON.stringify(data)
+        }
+        const res = await fetch(COMMENTS_API_URL, options)
+        const json_res = await res.json()
+        setComments(json_res)
+      }
+      useEffect(() => {
+        if (Object.keys(comment_data).length !== 0) {
+          postComment(comment_data)
+        }
+      }, [comment_data])
+  
+      const handleClick = async (event) => {
+        event.preventDefault()
+        setComment_data({
+          "name":event.target.name.value,
+          "comment":event.target.comment.value
+        })
+      }
+      
+      return (
+        <div className="CommentForm">
+          <form id="commentForm" onSubmit={handleClick}>
+            <input className="CommentForm-name" type="text" id="name" defaultValue="name"></input><br />
+            <textarea className="CommentForm-comment" id="comment" defaultValue="comment"></textarea><br />
+            <input className="CommentForm-submit" type="submit" value="post"></input>
+          </form>
+        </div>
+      )
+    }
+
     return (
       <div className="Comments">
         {Object.keys(comments).map((key, index) => (
@@ -27,43 +63,12 @@ function App() {
             <div className = "Comments-created_date">{new Date(Date.parse(comments[key]["created_date"])).toLocaleString(navigator.language)}</div>
           </div>
         ))}
+      <CommentForm />
       </div>
     );
   }
 
-  function CommentForm() {
-    const [comment_data, setComment_data] = useState({})
-    const postComment = async (data) => {
-      const options = {
-        method: 'POST',
-        body: JSON.stringify(data)
-      }
-      fetch(COMMENTS_API_URL, options)
-    }
-    useEffect(() => {
-      if (Object.keys(comment_data).length !== 0) {
-        postComment(comment_data)
-      }
-    }, [comment_data])
 
-    const handleClick = async (event) => {
-      event.preventDefault()
-      setComment_data({
-        "name":event.target.name.value,
-        "comment":event.target.comment.value
-      })
-    }
-    
-    return (
-      <div className="CommentForm">
-        <form id="commentForm" onSubmit={handleClick}>
-          <input className="CommentForm-name" type="text" id="name" defaultValue="name"></input><br />
-          <textarea className="CommentForm-comment" id="comment" defaultValue="comment"></textarea><br />
-          <input className="CommentForm-submit" type="submit" value="post"></input>
-        </form>
-      </div>
-    )
-  }
   return (
     <div className="App">
       <header className="App-header">
@@ -71,7 +76,7 @@ function App() {
         <h1>BAGPYP</h1>
       </header>
       <Comments />
-      <CommentForm />
+      {/* <CommentForm /> */}
     </div>
   );
 }
